e2db3fdcec19c3d5da4e58d9b055356aa10236e0
stage <stage@vista.it>
2019-05-14 17:09:38 +0200

    aggiunto come configurare aruba business

---
+++ aruba_config.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+   <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5N7JMZ');</script>
+<!-- End Google Tag Manager -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="Progettazione e sviluppo siti internet, e-commerce, grafica web e cartacea a Ravenna. Creazione applicazioni per smartphone e tablet.">
+
+    <title>Vista Tecnologie - Realizzazione siti web, grafica web e cartacea, applicazioni mobile a Ravenna</title>
+
+    <script src="/assets/js/plugins/retina/retina.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,600,500,700,800,900' rel='stylesheet' type='text/css'>
+
+   	<link href="/assets/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
+   	<link href="/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+   	<link href="/assets/css/plugins/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css">
+   	<link href="/assets/css/plugins/owl-carousel/owl.theme.css" rel="stylesheet" type="text/css">
+   	<link href="/assets/css/plugins/owl-carousel/owl.transitions.css" rel="stylesheet" type="text/css">
+   	<link href="/assets/css/plugins/magnific-popup.css" rel="stylesheet" type="text/css">
+   	<link href="/assets/css/plugins/jquery.fs.wallpaper.css" rel="stylesheet" type="text/css">
+   	<link href="/assets/css/palette.css" rel="stylesheet" type="text/css">
+   	<link href="/assets/css/vista.css?1" rel="stylesheet" type="text/css">
+   	<link href="/assets/css/map-icons.css" rel="stylesheet" type="text/css">
+
+
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body id="page" itemscope itemtype="http://schema.org/LocalBusiness" >
+   <!-- Google Tag Manager (noscript) -->
+   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N7JMZ"
+   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+   <!-- End Google Tag Manager (noscript) -->
+   <nav class="navbar navbar-inverse navbar-fixed-top navbar-expanded">
+        <div class="container">
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                    <img src="assets/img/logo.png" class="img-responsive" alt="Vista Tecnologie">
+                </a>
+                <h1 class="hide" itemprop="name" >Vista tecnologie s.r.l.</h1>
+                <meta itemprop="url" content="http://www.vista.it" />
+    			<meta itemprop="image" content="http://www.vista.it/assets/img/logo.png" />
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#process">Lavora con noi</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section id="process" class="services">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <h2>Configurazione Aruba business</h2>
+                    <hr class="colored">
+                </div>
+            </div>
+            <div class="row content-row">
+                <div class="col-md-12">
+                    <div class="media">
+                        <div class="pull-left">
+                            <i class="fa fa-clipboard"></i>
+                        </div>
+                        <div class="media-body">
+                            <h3 class="media-heading">Casella aruba business su client di posta e dispositivi mobile</h3>
+                            <table>
+                              <tr>
+                                <td>
+                                  <br><br><br>
+                                  <h4>CONFIGURAZIONE POP3S</h4>
+                                  <p>Questo tipo di configurazione consente la trasmissione delle informazioni in modalità cifrata.</p>
+                                  <h4>Impostazioni account</h4>
+                                  <ul>
+                                    <li>Nome: Mario Rossi</li>
+                                    <li>E-mail: indirizzo di posta (es: nomecasella@nomedominio.xxx)</li>
+                                    <li>Password: password legata alla casella</li>
+                                    <li>Tipo di account: POP3S</li>
+                                  </ul>
+                                  <h4>Impostazioni server di posta in arrivo</h4>
+                                  <ul>
+                                    <li>Nome Host: in.postassl.it</li>
+                                    <li>Nome Utente: indirizzo di posta (es: nomecasella@nomedominio.xxx)</li>
+                                    <li>Password: la password legata alla casella</li>
+                                    <li>Usa SSL: Attivato</li>
+                                    <li>Autenticazione: Password</li>
+                                    <li>Porta Server: 995</li>
+                                  </ul>
+                                  <h4>Impostazioni server di posta in uscita</h4>
+                                  <ul>
+                                    <li>Nome Host: out.postassl.it</li>
+                                    <li>Nome Utente: indirizzo di posta (es: nomecasella@nomedominio.xxx)</li>
+                                    <li>Password: la password legata alla casella</li>
+                                    <li>Usa SSL: Attivato</li>
+                                    <li>Autenticazione: Password</li>
+                                    <li>Porta Server: 465</li>
+                                  </ul>
+                                </td>
+                            <!--</tr>-->
+
+                            <!--<tr>-->
+                                <td>
+                                  <h4>CONFIGURAZIONE IMAP4S</h4>
+                                  <p>Il protocollo IMAP4S consente la condivisione di tutte le cartelle di una stessa casella di posta tra più utenti e/o dispositivi.</p>
+                                  <h4>Impostazioni account</h4>
+                                  <ul>
+                                    <li>Nome: Mario Rossi</li>
+                                    <li>E-mail: indirizzo di posta (es: nomecasella@nomedominio.xxx)</li>
+                                    <li>Password: la password legata alla casella</li>
+                                    <li>Tipo di Account: IMAP4S</li>
+                                  </ul>
+                                  <h4>Impostazioni server di posta in arrivo</h4>
+                                  <ul>
+                                    <li>Nome Host: in.postassl.it</li>
+                                    <li>Nome Utente: indirizzo di posta (es: nomecasella@nomedominio.xxx)</li>
+                                    <li>Password: la password legata alla casella</li>
+                                    <li>Usa SSL: Attivato</li>
+                                    <li>Autenticazione: Password</li>
+                                    <li>Porta Server: 993</li>
+                                  </ul>
+                                  <h4>Impostazioni server di posta in uscita</h4>
+                                  <ul>
+                                    <li>Nome Host: out.postassl.it</li>
+                                    <li>Nome Utente: indirizzo di posta (es: nomecasella@nomedominio.xxx)</li>
+                                    <li>Password: la password legata alla casella</li>
+                                    <li>Usa SSL: Attivato</li>
+                                    <li>Autenticazione: Password</li>
+                                    <li>Porta Server: 465</li>
+                                  </ul>
+                                </td>
+                              </tr>
+                            </table>
+
+                            <p>
+                            	<a href="mailto:commerciale@vista.it?subject=Lavora con noi - Junior Developer">Invia CV</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+        	</div>
+        </div>
+    </section>
+
+    <footer class="footer" style="background-image: url('assets/img/bg-footer.jpg')">
+
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-4 contact-details">
+                    <h4 class="azz"><i class="fa fa-phone"></i> Chiamaci</h4>
+                    <p itemprop="telephone">0544500905</p>
+                </div>
+                <div class="col-md-4 contact-details" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                    <h4 class="fux"><i class="fa fa-map-marker"></i> Vieni a trovarci</h4>
+                    <p><span itemprop="streetAddress">Via Romea 150 R</span><br><span  itemprop="postalCode" >48121</span> <span itemprop="addressLocality">Ravenna</span></p>
+                    <meta itemprop="addressCountry" content="Italy" />
+                </div>
+                <div class="col-md-4 contact-details">
+                    <h4 class="yel"><i class="fa fa-envelope"></i> Scrivici</h4>
+                    <p><a itemprop="email" href="mailto:feedback@startbootstrap.com">commerciale@vista.it</a>
+                    </p>
+                </div>
+            </div>
+            <div class="row social hide">
+                <div class="col-lg-12">
+                    <ul class="list-inline">
+                        <li><a href="#"><i class="fa fa-facebook fa-fw fa-2x"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-twitter fa-fw fa-2x"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-linkedin fa-fw fa-2x"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row copyright">
+                <div class="col-lg-12">
+                    <p class="small">&copy; 2014 Vista Tecnologie s.r.l. P.IVA 02487080398<br>
+					<a href="#" class="cookie-link-to-privacy">Privacy policy</a></p>
+                </div>
+                <div class="col-md-12 text-center links">
+						<span><i class="fa fa-info-circle"></i> <a href="/siti-web-ravenna.htm">Realizzazione siti web a Ravenna</a></span>
+				</div>
+            </div>
+        </div>
+    </footer>
+
+     <!-- Work Modal -->
+    <div class="portfolio-modal modal fade" id="workModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-body"><!--  --></div>
+        </div>
+    </div>
+
+
+
+    <!-- Core Scripts -->
+
+    <script src="/assets/js/jquery-1.10.2.js"></script>
+    <script src="/assets/js/bootstrap/bootstrap.min.js"></script>
+    <script src="/assets/js/plugins/jquery.easing.min.js"></script>
+    <script src="/assets/js/plugins/classie.js"></script>
+    <script src="/assets/js/plugins/cbpAnimatedHeader.js"></script>
+    <script src="/assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+    <script src="/assets/js/plugins/jquery.magnific-popup/jquery.magnific-popup.js"></script>
+    <script src="/assets/js/plugins/jquery.fs.wallpaper.js"></script>
+    <script src="/assets/js/contact_me.js"></script>
+    <script src="/assets/js/plugins/jqBootstrapValidation.js"></script>
+    <script src="/assets/js/home.js"></script>
+
+    <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script src="/assets/js/plugins/jquery.gmap.min.js"></script>
+    <script src="/assets/js/map.js"></script>
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-12985628-1', 'vista.it');
+	  ga('send', 'pageview');
+	</script>
+	<ul class="hide">
+		<li><a href="ita_index.php" >Prevista ita</a></li>
+		<li><a href="eng_index.php" >Prevista eng</a></li>
+	</ul>
+
+	<script type="text/javascript" src="http://cookie-privacy.vista.it/vistatecnologie/cookies-policy.js"></script>
+</body>
+
+</html>
